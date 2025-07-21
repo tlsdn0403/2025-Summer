8a28d5ab71eacded3616d18893220c3b39882176
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-07-21 14:13:42 +0900

    .

---
+++ sinwoo/Unreal/Sector4/Intermediate/Build/Win64/UnrealEditor/Inc/Sector4/UHT/Grabber.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Grabber.h"
+
+#ifdef SECTOR4_Grabber_generated_h
+#error "Grabber.generated.h already included, missing '#pragma once' in Grabber.h"
+#endif
+#define SECTOR4_Grabber_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class UGrabber *****************************************************************
+SECTOR4_API UClass* Z_Construct_UClass_UGrabber_NoRegister();
+
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector4_Source_Sector4_Grabber_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUGrabber(); \
+	friend struct Z_Construct_UClass_UGrabber_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend SECTOR4_API UClass* Z_Construct_UClass_UGrabber_NoRegister(); \
+public: \
+	DECLARE_CLASS2(UGrabber, USceneComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Sector4"), Z_Construct_UClass_UGrabber_NoRegister) \
+	DECLARE_SERIALIZER(UGrabber)
+
+
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector4_Source_Sector4_Grabber_h_13_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	UGrabber(UGrabber&&) = delete; \
+	UGrabber(const UGrabber&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGrabber); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGrabber); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UGrabber) \
+	NO_API virtual ~UGrabber();
+
+
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector4_Source_Sector4_Grabber_h_10_PROLOG
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector4_Source_Sector4_Grabber_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector4_Source_Sector4_Grabber_h_13_INCLASS_NO_PURE_DECLS \
+	FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector4_Source_Sector4_Grabber_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class UGrabber;
+
+// ********** End Class UGrabber *******************************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector4_Source_Sector4_Grabber_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
