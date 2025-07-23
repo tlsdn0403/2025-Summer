9576076d91b285d8703828d0b1580368a9a3551d
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-07-23 14:17:40 +0900

    몰라 그냥 지울래

---
+++ sinwoo/Unreal/Sector2/Intermediate/Build/Win64/UnrealEditor/Inc/Sector2/UHT/NewSceneComponent.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "NewSceneComponent.h"
+
+#ifdef SECTOR2_NewSceneComponent_generated_h
+#error "NewSceneComponent.generated.h already included, missing '#pragma once' in NewSceneComponent.h"
+#endif
+#define SECTOR2_NewSceneComponent_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class UNewSceneComponent *******************************************************
+SECTOR2_API UClass* Z_Construct_UClass_UNewSceneComponent_NoRegister();
+
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector2_Source_Sector2_NewSceneComponent_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUNewSceneComponent(); \
+	friend struct Z_Construct_UClass_UNewSceneComponent_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend SECTOR2_API UClass* Z_Construct_UClass_UNewSceneComponent_NoRegister(); \
+public: \
+	DECLARE_CLASS2(UNewSceneComponent, USceneComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Sector2"), Z_Construct_UClass_UNewSceneComponent_NoRegister) \
+	DECLARE_SERIALIZER(UNewSceneComponent)
+
+
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector2_Source_Sector2_NewSceneComponent_h_13_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	UNewSceneComponent(UNewSceneComponent&&) = delete; \
+	UNewSceneComponent(const UNewSceneComponent&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UNewSceneComponent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UNewSceneComponent); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UNewSceneComponent) \
+	NO_API virtual ~UNewSceneComponent();
+
+
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector2_Source_Sector2_NewSceneComponent_h_10_PROLOG
+#define FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector2_Source_Sector2_NewSceneComponent_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector2_Source_Sector2_NewSceneComponent_h_13_INCLASS_NO_PURE_DECLS \
+	FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector2_Source_Sector2_NewSceneComponent_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class UNewSceneComponent;
+
+// ********** End Class UNewSceneComponent *********************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_tlsdn_Desktop_2025_Summer_sinwoo_Unreal_Sector2_Source_Sector2_NewSceneComponent_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
