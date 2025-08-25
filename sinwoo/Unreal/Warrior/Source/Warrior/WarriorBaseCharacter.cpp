f165f5e45039c4c13b76d2093b43e8a033a486a0
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-08-25 12:42:01 +0900

    클래스 생성
    캐릭터 베이스 , 베이스 게임모드

---
+++ sinwoo/Unreal/Warrior/Source/Warrior/WarriorBaseCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WarriorBaseCharacter.h"
+
+// Sets default values
+AWarriorBaseCharacter::AWarriorBaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWarriorBaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWarriorBaseCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AWarriorBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
