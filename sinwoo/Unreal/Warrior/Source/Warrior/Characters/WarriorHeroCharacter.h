533caeec6624cef731be65dfea9c99fdc463f18a
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-08-25 14:26:40 +0900

    정리

--- sinwoo/Unreal/Warrior/Source/Warrior/Characters/WarriorHeroCharacter.h
+++ sinwoo/Unreal/Warrior/Source/Warrior/Characters/WarriorHeroCharacter.h
@@ -14,6 +14,23 @@ class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	AWarriorHeroCharacter();
+
 protected:
 	virtual void BeginPlay() override;	
+
+
+private:
+#pragma region Components
+
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+#pragma endregion Components
 };
