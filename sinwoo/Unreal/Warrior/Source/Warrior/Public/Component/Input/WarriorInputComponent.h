b5536e7d2b4a8f498db834b7583efeb839f1f5f1
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2025-08-27 02:48:51 +0900

    인풋컴포넌트 커스텀

--- sinwoo/Unreal/Warrior/Source/Warrior/Public/Component/Input/WarriorInputComponent.h
+++ sinwoo/Unreal/Warrior/Source/Warrior/Public/Component/Input/WarriorInputComponent.h
@@ -27,5 +27,10 @@ class WARRIOR_API UWarriorInputComponent : public UEnhancedInputComponent
 template<class UserObject, typename CallbackFunction>
 inline void UWarriorInputComponent::BindNativeAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TrrigerEvent, UserObject* ContextObject, CallbackFunction Func)
 {
-	check(InInputConfig);
+	checkf(InInputConfig, TEXT("InInput Config data asset is null"));  //디버깅 할 떄 도움을 주기 위해서 사용
+
+	if (UInputAction* FindInputAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FindInputAction, TrrigerEvent, ContextObject, Func);
+	}
 }
